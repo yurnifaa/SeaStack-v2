732682ddeb1f375e4268f01da18e1b6d3b447125
Jana Del Rosario <delrosariojana9@gmail.com>
2026-05-04 21:40:11 +0800

    render -> railway

--- backend/server.py
+++ backend/server.py
@@ -493,4 +493,5 @@ class _CompileError(Exception):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
